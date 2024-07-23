53adac1f7c291ff3d253e83e170577d4e0e772ce
Kanchanprajapat <141110309+Kanchanprajapat@users.noreply.github.com>
2024-07-23 10:48:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 function Person(name, age) {
     this.name = name;
     this.age = age;
-    this.greet = function() {
+	greet() {
         console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
     }
 }
@@ -10,7 +10,7 @@ function Person(name, age) {
 function Employee(name, age, jobTitle) {
     Person.call(this, name, age);
     this.jobTitle = jobTitle;
-    this.jobGreet = function() {
+    jobGreet() {
         console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
     }
 }
